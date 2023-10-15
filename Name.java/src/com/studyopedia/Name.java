4a458befa96087303194ee01c2efabee2790c0bc
User <User@LAPTOP-QC69QVEV>
2023-10-16 00:47:28 +0800

    Nurrina Fassha

---
+++ Name.java/src/com/studyopedia/Name.java
@@ -0,0 +1,13 @@
+package com.studyopedia;
+
+public class Name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+			System.out.println("Nurrina Fassha");
+		
+		
+	}
+
+}
